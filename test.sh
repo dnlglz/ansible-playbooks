edb142d0711ada23872ccab2fb73a56d5e5c1620
Daniel González <gonzalez.lopez.daniel@gmail.com>
2017-07-19 09:28:32 +0200

    Add timer to tests

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -v -c local site.yml -K --become-method=sudo --tags test
+time ansible-playbook -v -c local site.yml -K --become-method=sudo --tags test
--- test_all.sh
+++ test_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -v -c local site.yml -K --become-method=sudo
+time ansible-playbook -v -c local site.yml -K --become-method=sudo
