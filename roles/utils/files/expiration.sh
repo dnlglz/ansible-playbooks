b5dc954eb9285bab92ecf74b372d4cbc5d9d54bf
Daniel González <gonzalez.lopez.daniel@gmail.com>
2018-11-16 11:01:56 +0100

    Help message for non root user when execute expiration.sh

--- roles/utils/files/expiration.sh
+++ roles/utils/files/expiration.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "sudo -u root expiration.sh | less"
+    exit 1
+fi
+
 cd /etc/letsencrypt/live/
 for domain in *; do echo $domain; openssl x509 -dates -noout < $domain/cert.pem; done
