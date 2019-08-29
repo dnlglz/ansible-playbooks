d6afffc9a1d15d8c2b63a2cce07e225d54e33cfb
Daniel González <gonzalez.lopez.daniel@gmail.com>
2019-08-29 12:02:48 +0200

    Add mail subdomain to letsencrypt script

--- roles/utils/files/letsenc.sh
+++ roles/utils/files/letsenc.sh
@@ -1,6 +1,6 @@
-#!/bin/bash - 
+#!/bin/bash -
 
 set -o nounset                              # Treat unset variables as an error
 
-sudo certbot certonly --manual -d $1 -d www.$1
+sudo certbot certonly --manual -d $1 -d www.$1 -d mail.$1
 
