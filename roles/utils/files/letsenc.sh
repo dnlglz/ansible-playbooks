178f7c8acb5095439de936fa7350f2d4e29c159c
Daniel González <gonzalez.lopez.daniel@gmail.com>
2019-11-12 09:00:44 +0100

    Delete mail subdomain in letsench.sh

--- roles/utils/files/letsenc.sh
+++ roles/utils/files/letsenc.sh
@@ -2,5 +2,5 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-sudo certbot certonly --manual -d $1 -d www.$1 -d mail.$1
+sudo certbot certonly --manual -d $1 -d www.$1 
 
